ef8c86e176327953a16ecc0d5bf82975aa7102e9
2xSerious <79917801+2xSerious@users.noreply.github.com>
2021-11-01 10:48:39 +0000

    changes

--- src/js/helpers.js
+++
@@ -1,9 +0,0 @@
-exports.getDate = () => {
-  let d = new Date();
-  let yyyy = d.getFullYear();
-  let mm = d.getMonth() + 1;
-  let dd = d.getDate();
-
-  let createdAt = `${dd}-${mm}-${yyyy}`;
-  return createdAt;
-};
